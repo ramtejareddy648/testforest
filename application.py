bf49733b1c53fa7fe16ecfe4acb31341b525ccbf
reddy <ramtejareddy2@gmail.com>
2025-02-27 17:43:27 +0530

    first commit

---
+++ application.py
@@ -0,0 +1,43 @@
+from flask import Flask,request,render_template,jsonify
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+import pickle
+
+
+application=Flask(__name__)
+app=application
+
+ridge_model=pickle.load(open('models/ridge.pkl',"rb"))
+standard_scaler=pickle.load(open("models/scaler.pkl","rb"))
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/predict",methods=["GET",'POST'])
+def predict_data():
+    if request.method=='POST':
+        temperature=float(request.form.get("temperature"))
+        RH=float(request.form.get("RH"))
+        Ws=float(request.form.get("Ws"))
+        Rain=float(request.form.get("Rain"))
+        FFMC=float(request.form.get("FFMC"))
+        DMC=float(request.form.get("DMC"))
+        ISI=float(request.form.get("ISI"))
+        Clases=float(request.form.get("Clases"))
+        Region=float(request.form.get("Region"))
+        new_data_scale=standard_scaler.transform([[temperature,RH,Ws,Rain,FFMC,DMC,ISI,Clases,Region]])
+        result=ridge_model.predict(new_data_scale)
+        return render_template('home.html',results=result[0])
+        
+        
+    else:
+        return render_template('home.html')
+
+
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0")
